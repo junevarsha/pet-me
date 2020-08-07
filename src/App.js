08f970a2b09f139ce32ef042c5d3fc3d928fb38c
Varsha Thirumakil <varsht612@gmail.com>
2020-08-06 21:11:56 -0400

    basic search param

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
 
-ReactDOM.render(<App />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
